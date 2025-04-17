8a7ffc16acfe00522cb0f2ff7a1589bd5bbf0788
EduaardoFerrari <eduardof_amigo@hotmail.com>
2025-04-17 10:35:48 -0300

    Segundo commit

---
+++ funcoes.php
@@ -0,0 +1,18 @@
+<?php
+
+$num1 = 1;
+$num2 = 2;
+function soma($num1, $num2): mixed{
+    return($num1 + $num2);
+    echo "$return";
+}
+
+function dividir($n1, $n2): mixed{
+    return($n1 / $n2);
+}
+
+function raiz($numero): mixed{
+    return($numero ** 0.5);
+}
+
+?>
